50b9d649b53d9dc2c6098a08ea0f364faa4a31ed
AntonZHS <ant912377@gmail.com>
2022-07-14 13:36:03 +0300

    beginnen test program

---
+++ guess.py
@@ -0,0 +1,10 @@
+import random
+
+guessesTaken = 0
+
+print('Привет! Как тебя зовут?')
+
+myName = input()
+
+number = random.randit(1, 20)
+print('Что ж, ' + myName + ', я загадываю число от 1 до 20.')
